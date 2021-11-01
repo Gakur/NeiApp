2f9519b307cba73a4d6eda688a09e9d54a96c776
Peter Gakure <petergakure97@gmail.com>
2021-11-02 02:39:03 +0300

    added profiles model

--- Hood/models.py
+++ Hood/models.py
@@ -70,3 +70,15 @@ class Post(models.Model):
 
     def delete_post(self):
         self.delete
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=254, blank=True)
+    image =CloudinaryField(default='default.jpg')
+    hood = models.ForeignKey(Neighbourhood, on_delete=models.SET_NULL, null=True, related_name='users', blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} profile'
+    
+    
