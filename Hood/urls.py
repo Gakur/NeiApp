ca34c36dce0d53eeb723eb0a99a047a575115f67
Peter Gakure <petergakure97@gmail.com>
2021-11-03 17:55:46 +0300

    create search, profile and post paths

--- Hood/urls.py
+++ Hood/urls.py
@@ -6,6 +6,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('location/', views.location, name = 'hood'),
+    path('location/', views.location, name = 'location'),
+        path('add_biz/', views.add_biz, name = 'add_biz'),
+    path('Post/<int:id>/create-post/',views.Post, name='post'),
+
+    path('create_profile/',views.create_profile,name = 'create_profile'),
+    path('EditProfile/<username>/',views.EditProfile,name = 'EditProfile'),
+    path('estate/<int:id>/', views.estate, name = 'each-hood'),
+    path('search/', views.search, name = 'search'),
 
 ]
